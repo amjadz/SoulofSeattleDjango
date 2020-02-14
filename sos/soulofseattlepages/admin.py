b549855bfc0d75956e64606f42d5be69bff69540
Ethan K. Chung <55211057+Ethan-Chung@users.noreply.github.com>
2020-02-14 00:06:51 -0800

    fixed database

--- sos/soulofseattlepages/admin.py
+++ sos/soulofseattlepages/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
-from .models import Post
+from .models import author, userPost
 
 # Register your models here.
 
+class userAdminPost(admin.ModelAdmin):
+    list_display = ('post_Title', 'post_Slug', 'post_Status', 'post_CreatedOn')
+    list_filter = ("post_Status")
+    search_fields = ['post_Title', 'post_Content']
+    prepopulated_fields = {'post_Slug': ('post_Title')}
 
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'status','created_on')
-    list_filter = ("status",)
-    search_fields = ['title', 'content']
-    prepopulated_fields = {'slug': ('title',)}
+class userAuthorAdminPost(admin.ModelAdmin):
+    list_display = ('author_firstName', 'author_lastName')
 
+admin.site.register(author)
+admin.site.register(userPost)
 
-admin.site.register(Post)
